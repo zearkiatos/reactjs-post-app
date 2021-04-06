fecfe5d65fc64472704107b88de17e79918252f0
zearkiatos <caprilespe@outlook.com>
2021-04-06 19:45:10 -0400

    More on Async Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import TYPES from '../types';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
-    const response = await jsonPlaceholder.get('/posts');
+export const fetchPosts = () => {
+    const promise = jsonPlaceholder.get('/posts');
     return {
         type: TYPES.FETCH_POSTS,
-        payload: response
+        payload: promise
     };
 };
