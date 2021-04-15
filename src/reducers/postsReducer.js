c632f1be7d71c5e34266265f72f99b7386f1a187
pcapriles <pedro.capriles@walmart.com>
2021-04-15 08:06:52 -0400

    Switch statement in Reducers

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,10 @@
-
-export default (state, action) => {
+import TYPES from "../types";
+export default (state = [], action) => {
+  switch (action.type) {
+    case TYPES.FETCH_POSTS: {
+      return action.payload;
+    }
+    default:
+      return state;
+  }
 };
