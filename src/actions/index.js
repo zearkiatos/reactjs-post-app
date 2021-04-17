e0aeeb8e63e465854e12365eb3c956977f9d3388
zearkiatos <caprilespe@outlook.com>
2021-04-17 10:20:54 -0400

    List Building

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,19 @@
 import TYPES from '../types';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import faker from 'faker'
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: TYPES.FETCH_POSTS, payload: response.data })
+    const mappedPosts = mapperPosts(response.data)
+
+    dispatch({ type: TYPES.FETCH_POSTS, payload: mappedPosts })
 };
+
+const mapperPosts = posts => posts.map(post => ({
+    ...post,
+    'avatar': faker.image.avatar
+}))
 
 export const selectPost = () => {
     return {
